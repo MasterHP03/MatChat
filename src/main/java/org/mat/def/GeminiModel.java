1dca42a1905c85e16f1fbc2a785aee5ca324cf39
Matsalter <geniusyhp@gmail.com>
2026-07-22 00:39:36 +0900

    feat: support Gemini 3.5 Flash Lite and Gemini 3.6 Flash

--- src/main/java/org/mat/def/GeminiModel.java
+++ src/main/java/org/mat/def/GeminiModel.java
@@ -3,8 +3,10 @@ package org.mat.def;
 public enum GeminiModel {
     PRO31("gemini-3.1-pro-preview"),
     // PRO3("gemini-3-pro-preview"),
+    FLASH36("gemini-3.6-flash"),
     FLASH35("gemini-3.5-flash"),
     FLASH3("gemini-3-flash-preview"),
+    FLASHLITE35("gemini-3.5-flash-lite"),
     FLASHLITE31("gemini-3.1-flash-lite"),
     PRO25("gemini-2.5-pro"),
     FLASH25("gemini-2.5-flash"),
